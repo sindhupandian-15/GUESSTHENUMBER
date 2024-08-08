70528b2a8bd30a05e74ac5ec04f941279ae3bf0b
sindhupandian-15 <sindhu15402@gmail.com>
2024-08-08 13:44:03 +0530

    Add files via upload

---
+++ EMC/jsexample.js
@@ -0,0 +1,22 @@
+ //RESEVERSE NUMBER
+
+ for(n=10;n>=1;n=n-1)
+ {
+     console.log(n)
+ }
+
+    //EVEN NUMBERS
+ for ( i = 1; i <= 10; i++) {
+     if (i % 2 == 0) {
+         console.log(i);
+     }
+ }
+
+  //2 TABLE 
+
+  for(n=1;n<=10;n++){
+    console.log("2 * "+n +"="+n*2)
+  }
+
+
+
